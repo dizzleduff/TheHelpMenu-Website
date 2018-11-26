1c8c215f1143f36332261d2ea90573f1d181fa35
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2018-11-25 19:44:54 -0500

    calendar edit

---
+++ js/calendar.js
@@ -0,0 +1,56 @@
+$(function() {
+      // page is now ready, initialize the calendar...
+
+      $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: '2018-10-23',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [{
+            title: 'First Day of Fall Semester',
+            start: '2018-08-27'
+          },
+          {
+            title: 'Mile Test',
+            start: '2018-09-05T12:30:00',
+            end: '2018-09-05T13:30:00'
+          },
+          {
+            title: 'Team Hero Practice',
+            start: '2018-09-05T14:30:00',
+            end: '2018-09-05T16:30:00'
+          },
+          {
+            title: 'UA Sports Festival',
+            start: '2018-10-11',
+          },
+          {
+            title: 'Trip to Hero Museum',
+            start: '2018-10-16',
+          },
+          {
+            title: 'Presentation',
+            start: '2018-10-20T12:00:00',
+          },
+          {
+            title: 'Self Training',
+            start: '2018-10-20T16:00:00',
+          },
+          {
+            title: 'Internship',
+            start: '2018-10-22',
+            end: '2018-10-27'
+          },
+          {
+            title: 'Last Day of Fall Semester',
+            start: '2018-11-16',
+          },
+        ]
+      });
+    });
